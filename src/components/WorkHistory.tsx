d5a273e0caecb5ae515f67a5541c309de4bbe146
Rudolph Christian Razul <razulchristian@gmail.com>
2024-04-11 00:28:41 +0800

    Refactor WorkHistory component to display company address, update date format, and improve styling

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -10,15 +10,26 @@ const WorkHistory = ({ workHistory }: WorkHistoryProps) => {
       <h1 className=" bg-orange-400 py-1 pl-4 text-xl font-bold">
         WORK HISTORY
       </h1>
-      <div className="flex w-full flex-col">
+      <div className="flex w-full flex-col p-4">
         {workHistory.map((work, index) => (
           <div key={index} className="flex w-full flex-col">
-            <h2>{work.company}</h2>
-            <p>{work.address}</p>
-            <p>
-              {work.startDate.getDate()} {work.endDate.getDate()}
-            </p>
-            <p>{work.role}</p>
+            <div className="flex justify-between">
+              <h2 className="font-bold ">
+                {work.company} | {work.address}
+              </h2>
+              <p>
+                {work.startDate.toLocaleDateString("en-US", {
+                  month: "long",
+                  year: "numeric",
+                })}
+                {" - "}
+                {work.endDate.toLocaleDateString("en-US", {
+                  month: "long",
+                  year: "numeric",
+                })}
+              </p>
+            </div>
+            <p className="mb-1 font-bold text-primary">{work.role}</p>
             <p>{work.description}</p>
           </div>
         ))}
