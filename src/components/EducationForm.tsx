86f330f3ae03b60391fef7e4fa2e6aed40bc4a26
Rudolph Christian Razul <razulchristian@gmail.com>
2024-04-13 00:50:51 +0800

    Finish styling Education Form

--- src/components/EducationForm.tsx
+++ src/components/EducationForm.tsx
@@ -8,14 +8,20 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { CalendarIcon, Trash } from "lucide-react";
+import { CalendarIcon, ChevronDown } from "lucide-react";
 import { useFieldArray, useForm } from "react-hook-form";
 import { z } from "zod";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Calendar } from "./ui/calendar";
 import { format } from "date-fns";
 import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 const EducationSchema = z.object({
   school: z
@@ -54,6 +60,8 @@ interface EducationFormProps {
 }
 
 const EducationForm = ({ onEducation }: EducationFormProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+
   const form = useForm({
     resolver: zodResolver(EducationSchema),
     defaultValues: {
@@ -71,186 +79,211 @@ const EducationForm = ({ onEducation }: EducationFormProps) => {
     name: "school",
     control,
   });
-  return (
-    <div className="rounded-md border p-4 shadow-md">
-      <Form {...form}>
-        <div className="flex items-center justify-between">
-          <h1 className="text-xl font-bold">Education History</h1>
-          <Button
-            type="button"
-            onClick={() => {
-              append(defaultValues);
-            }}
-            variant="link"
-          >
-            Add..
-          </Button>
-        </div>
-        <form
-          onSubmit={handleSubmit((data) => {
-            console.log(data);
-          })}
-          className="space-y-2 py-2"
-        >
-          {fields.map((field, index) => {
-            return (
-              <div key={field.id} className="flex flex-col gap-2">
-                {/* School Name Field */}
-                <FormField
-                  name={`school.${index}.name`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <div className="flex justify-between">
-                        <FormLabel>School</FormLabel>
-                        <Button
-                          variant="link"
-                          onClick={() => remove(index)}
-                          className="h-0 text-red-500"
-                        >
-                          Remove
-                        </Button>
-                      </div>
-                      <FormControl>
-                        <Input
-                          placeholder="Enter School/University"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Location Field */}
-                <FormField
-                  name={`school.${index}.location`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>School Location</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter Location" {...field} />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Degree Field */}
-                <FormField
-                  name={`school.${index}.degree`}
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Degree</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="Enter Degree/Field of study"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                {/* Start Date and End Date Fields */}
-                <div className="flex gap-4">
-                  <FormField
-                    name={`school.${index}.startDate`}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Start Date</FormLabel>
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <FormControl>
-                              <Button
-                                variant={"outline"}
-                                className={cn(
-                                  "w-36 pl-3 text-left font-normal",
-                                  !field.value && "text-muted-foreground",
-                                )}
-                              >
-                                {field.value ? (
-                                  format(field.value, "P")
-                                ) : (
-                                  <span>Pick a date</span>
-                                )}
-                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                              </Button>
-                            </FormControl>
-                          </PopoverTrigger>
-                          <PopoverContent
-                            className="z-10 w-auto p-0"
-                            align="start"
-                          >
-                            <Calendar
-                              mode="single"
-                              selected={field.value}
-                              onSelect={field.onChange}
-                              disabled={(date) =>
-                                date > new Date() ||
-                                date < new Date("1900-01-01")
-                              }
-                              initialFocus
-                            />
-                          </PopoverContent>
-                        </Popover>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                  <FormField
-                    name={`school.${index}.endDate`}
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>End Date</FormLabel>
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <FormControl>
-                              <Button
-                                variant={"outline"}
-                                className={cn(
-                                  "w-36 pl-3 text-left font-normal",
-                                  !field.value && "text-muted-foreground",
-                                )}
-                              >
-                                {field.value ? (
-                                  format(field.value, "P")
-                                ) : (
-                                  <span>Pick a date</span>
-                                )}
-                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                              </Button>
-                            </FormControl>
-                          </PopoverTrigger>
-                          <PopoverContent
-                            className="z-10 w-auto p-0"
-                            align="start"
-                          >
-                            <Calendar
-                              mode="single"
-                              selected={field.value}
-                              onSelect={field.onChange}
-                              disabled={(date) =>
-                                date > new Date() ||
-                                date < new Date("1900-01-01")
-                              }
-                              initialFocus
-                            />
-                          </PopoverContent>
-                        </Popover>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-              </div>
-            );
-          })}
 
-          <p>{errors.school?.root?.message}</p>
-          <Button type="submit" variant="outline" className="self-end">
-            Submit
+  return (
+    <Collapsible
+      className="rounded-md border p-4 shadow-md"
+      open={isOpen}
+      onOpenChange={setIsOpen}
+    >
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold">Education History</h1>
+        <CollapsibleTrigger asChild>
+          <Button variant="ghost" size="sm" className="w-9 p-0">
+            <ChevronDown className="h-4 w-4" />
+            <span className="sr-only">Toggle</span>
           </Button>
-        </form>
-      </Form>
-    </div>
+        </CollapsibleTrigger>
+      </div>
+      <CollapsibleContent>
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit((data) => {
+              console.log(data);
+            })}
+            className="space-y-2"
+          >
+            {fields.map((field, index) => {
+              return (
+                <div
+                  key={field.id}
+                  className={
+                    index !== 0
+                      ? "flex flex-col gap-2 border-t-2 border-orange-500 py-4"
+                      : "flex flex-col gap-2 py-4"
+                  }
+                >
+                  {/* School Name Field */}
+                  <FormField
+                    name={`school.${index}.name`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <div className="flex justify-between">
+                          <FormLabel>School</FormLabel>
+                          <Button
+                            variant="link"
+                            onClick={() => remove(index)}
+                            className="h-0 text-red-500"
+                          >
+                            Remove
+                          </Button>
+                        </div>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter School/University"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Location Field */}
+                  <FormField
+                    name={`school.${index}.location`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>School Location</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter Location" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Degree Field */}
+                  <FormField
+                    name={`school.${index}.degree`}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Degree</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter Degree/Field of study"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* Start Date and End Date Fields */}
+                  <div className="flex gap-4">
+                    <FormField
+                      name={`school.${index}.startDate`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Start Date</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "w-36 pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground",
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "P")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="z-10 w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() ||
+                                  date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      name={`school.${index}.endDate`}
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>End Date</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "w-36 pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground",
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "P")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="z-10 w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                disabled={(date) =>
+                                  date > new Date() ||
+                                  date < new Date("1900-01-01")
+                                }
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+
+            <p>{errors.school?.root?.message}</p>
+            {/* CTA Buttons */}
+            <div className="flex justify-between">
+              {/* Submit Button */}
+              <Button type="submit" variant="default" className="self-end">
+                Submit
+              </Button>
+              {/* Add a School Button*/}
+              <Button
+                type="button"
+                onClick={() => {
+                  append(defaultValues);
+                }}
+                variant="secondary"
+              >
+                Add a school
+              </Button>
+            </div>
+          </form>
+        </Form>
+      </CollapsibleContent>
+    </Collapsible>
   );
 };
 
