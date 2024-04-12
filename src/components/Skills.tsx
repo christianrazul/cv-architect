4cc48355fa33b973ac76354e76bd999717870a8c
Rudolph Christian Razul <razulchristian@gmail.com>
2024-04-12 13:28:52 +0800

    Refactor: fix the Skills component and the initialization of the state in the parent component

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,7 +11,8 @@ const Skills = ({ skills }: SkillsProps) => {
       <h1 className=" bg-yellow-400 py-1 pl-4 text-xl font-bold">SKILLS</h1>
       <div className="grid w-full grid-cols-2 p-4">
         <ul>
-          {skills.skills ? (
+          {/* Check array length and if it's empty, render no skills listed */}
+          {skills.skills.length !== 0 ? (
             skills.skills.map((skill, index) => <li key={index}>• {skill}</li>)
           ) : (
             <li>No skills listed.</li>
